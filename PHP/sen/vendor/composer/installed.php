6303589a8810e2de2628ae672e98078b435ee71c
Roman Belčev <roman@belcev.cz>
2022-09-30 23:48:53 +0200

    nějaké základí balíčky

--- PHP/sen/vendor/composer/installed.php
+++ PHP/sen/vendor/composer/installed.php
@@ -10,6 +10,24 @@
         'dev' => true,
     ),
     'versions' => array(
+        'fortawesome/font-awesome' => array(
+            'pretty_version' => '6.2.0',
+            'version' => '6.2.0.0',
+            'reference' => 'd3a7818c253fcbafff9ebd1d4abb2866c192e1d7',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../fortawesome/font-awesome',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'jackocnr/intl-tel-input' => array(
+            'pretty_version' => '17.0.19',
+            'version' => '17.0.19.0',
+            'reference' => 'a21a1aee03bfd6b82a028704490547ff77d8bf98',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../jackocnr/intl-tel-input',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
         'latte/latte' => array(
             'pretty_version' => 'v2.11.5',
             'version' => '2.11.5.0',
@@ -207,6 +225,21 @@
             'install_path' => __DIR__ . '/../tracy/tracy',
             'aliases' => array(),
             'dev_requirement' => false,
+        ),
+        'twbs/bootstrap' => array(
+            'pretty_version' => 'v5.2.1',
+            'version' => '5.2.1.0',
+            'reference' => '23e50829f958ea1d741d63e2781716be037e4644',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../twbs/bootstrap',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'twitter/bootstrap' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => 'v5.2.1',
+            ),
         ),
     ),
 );
