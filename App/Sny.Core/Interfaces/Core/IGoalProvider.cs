91145c19e4e73741509ac5ce646e599f02f44a63
Michal Kavka <michal.kavka@ininvest.cz>
2022-10-01 17:57:02 +0200

    Filter goals by account

--- App/Sny.Core/Interfaces/Core/IGoalProvider.cs
+++ App/Sny.Core/Interfaces/Core/IGoalProvider.cs
@@ -9,7 +9,7 @@ namespace Sny.Core.Interfaces.Core
         public Task<IReadOnlyCollection<Goal>> GetGoals();
 
         public Task<Goal> AddGoal(string name, bool active, string description);
-        
+
         public Task<Goal> EditGoal(Guid id, string name, bool active, string description);
 
         public void DeleteGoal(Guid id);
--- App/Sny.Infrastructure/Services/Repos/GoalInmemoryRepo.cs
+++ App/Sny.Infrastructure/Services/Repos/GoalInmemoryRepo.cs
@@ -9,50 +9,50 @@ namespace Sny.Infrastructure.Services.Repos
 
         private List<Goal> _goals = new List<Goal>()
         {
-            new Goal(new Guid("8fcfc3fa-590d-462d-b063-dbac4e4b42b1"), "Cíl test1", true, "Tak ale toto je cool popisek."),
-            new Goal(new Guid("8fcfc3fa-590d-462d-b063-dbac4e4b42b2"), "Cíl test2", false, "Tak ale toto je cool popisek!2"),
-            new Goal(new Guid("8fcfc3fa-590d-462d-b063-dbac4e4b42b3"), "Cíl test3", false, "Tak ale toto je cool popisek!3"),
-            new Goal(new Guid("8fcfc3fa-590d-462d-b063-dbac4e4b42b4"), "Cíl test4", false, "Tak ale toto je cool popisek!4"),
-            new Goal(new Guid("8fcfc3fa-590d-462d-b063-dbac4e4b42b5"), "Cíl test5", true, "Tak ale toto je cool popisek!5"),
-            new Goal(new Guid("8fcfc3fa-590d-462d-b063-dbac4e4b42b6"), "Cíl test6", false, "Tak ale toto je cool popisek!6"),
+            new Goal(new Guid("8fcfc3fa-590d-462d-b063-dbac4e4b42b1"), "Cíl test1", true, "Tak ale toto je cool popisek.", new Guid("8fcfc3fa-590d-462d-b063-dbac4e4b42b1")),
+            new Goal(new Guid("8fcfc3fa-590d-462d-b063-dbac4e4b42b2"), "Cíl test2", false, "Tak ale toto je cool popisek!2", new Guid("8fcfc3fa-590d-462d-b063-dbac4e4b42b1")),
+            new Goal(new Guid("8fcfc3fa-590d-462d-b063-dbac4e4b42b3"), "Cíl test3", false, "Tak ale toto je cool popisek!3", new Guid("8fcfc3fa-590d-462d-b063-dbac4e4b42b1")),
+            new Goal(new Guid("8fcfc3fa-590d-462d-b063-dbac4e4b42b4"), "Cíl test4", false, "Tak ale toto je cool popisek!4", new Guid("8fcfc3fa-590d-462d-b063-dbac4e4b42b1")),
+            new Goal(new Guid("8fcfc3fa-590d-462d-b063-dbac4e4b42b5"), "Cíl test5", true, "Tak ale toto je cool popisek!5", new Guid("8fcfc3fa-590d-462d-b063-dbac4e4b42b4")),
+            new Goal(new Guid("8fcfc3fa-590d-462d-b063-dbac4e4b42b6"), "Cíl test6", false, "Tak ale toto je cool popisek!6", new Guid("8fcfc3fa-590d-462d-b063-dbac4e4b42b4")),
         };
 
-        public Task<Goal> AddGoal(string name, bool active, string description)
+        public Task<Goal> AddGoal(string name, bool active, string description, Guid accountId)
         {
-            var goal = new Goal(Guid.NewGuid(), name, active, description);
+            var goal = new Goal(Guid.NewGuid(), name, active, description, accountId);
             _goals.Add(goal);
             return Task.FromResult(goal);
         }
 
-        public Task<Goal> EditGoal(Guid id, string name, bool active, string description)
+        public Task<Goal> EditGoal(Guid id, string name, bool active, string description, Guid accountId, Func<IQueryable<Goal>, IQueryable<Goal>> filter)
         {
-            DeleteGoal(id);
-            var goal = new Goal(id, name, active, description);
+            DeleteGoal(id, filter);
+            var goal = new Goal(id, name, active, description, accountId);
             _goals.Add(goal);
             return Task.FromResult(goal);
         }
 
-        public async Task<Goal> GetGoalById(Guid id)
+        public async Task<Goal> GetGoalById(Guid id, Func<IQueryable<Goal>, IQueryable<Goal>> filter)
         {
-            var goal = _goals.SingleOrDefault(d => d.Id == id);
+            var goal = filter(_goals.AsQueryable()).SingleOrDefault(d => d.Id == id);
             return goal ?? throw new GoalNotFoundException();
         }
 
-        public async Task<IReadOnlyCollection<Goal>> GetGoals()
+        public async Task<IReadOnlyCollection<Goal>> GetGoals(Func<IQueryable<Goal>, IQueryable<Goal>> filter)
         {
-            return _goals;
+            return filter(_goals.AsQueryable()).ToArray();
         }
 
-        public void DeleteGoal(Guid id)
+        public void DeleteGoal(Guid id, Func<IQueryable<Goal>, IQueryable<Goal>> filter)
         {
-            var goal = GetGoalById(id);
+            var goal = GetGoalById(id, filter);
             _goals.Remove(goal.Result);
         }
 
-        public void ChangeActiveGoal(Guid id, bool activate)
+        public void ChangeActiveGoal(Guid id, bool activate, Func<IQueryable<Goal>, IQueryable<Goal>> filter)
         {
-            var goal = GetGoalById(id).Result;
-            DeleteGoal(id);
+            var goal = GetGoalById(id, filter).Result;
+            DeleteGoal(id, filter);
             goal.Active = activate;
             _goals.Add(goal);
         }
